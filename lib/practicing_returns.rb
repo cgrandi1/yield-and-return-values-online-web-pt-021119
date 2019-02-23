621696fd5c1be8e1e58646f17f8332d323d5e5b0
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-23 01:42:09 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,12 +3,12 @@ require 'pry'
 def hello(array)
   i = 0
   while i < array.length
+    binding.pry
     yield(array[i])
     i += 1
   end
-binding.pry 
 end
 
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
